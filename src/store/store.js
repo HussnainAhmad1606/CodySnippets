d4a6dac39e59c967c146663a3af765b16e01be69
Uzair Sarfraz <116909186+UzairSarfraz@users.noreply.github.com>
2023-10-08 14:18:37 +0500

    store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,6 @@ import { create } from 'zustand'
 export const useUserStore = create((set) => ({
   username: "",
   email: "",
-  setUsername: (newUsername) => set({ username:newUsername }),
+  setUsername: (newUsername) => set({username:newUsername}),
   setEmail: (newEmail) => set({ email:newEmail })
 }))
