e52a9f898c297cc584d5c189a1fb5cd26f94642c
PsychoZexta <zohaib.saeed1259@gmail.com>
2023-10-14 12:18:34 +0500

    Added Categories Section

---
+++ src/components/Categories.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import CategoryCard from './CategoryCard'
+
+const Categories = () => {
+    const categories = [
+        {
+            id: 1,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 2,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 3,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 4,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 5,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 6,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 7,
+            name: 'HTML',
+            img: '/HTML.png',
+        },
+        {
+            id: 8,
+            name: 'HTML',
+            img: '/HTML.png',
+        }
+    ]
+    const Categories = categories.map(category => <CategoryCard key={category.name} name={category.name} img={category.img} />)
+    return (
+        <div>
+            <h1 className='text-4xl my-8 text-center'>Categories</h1>
+            <div className='grid w-full justify-center gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+                {Categories}
+            </div>
+        </div>
+    )
+}
+
+export default Categories
---
+++ src/components/CategoryCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CategoryCard = (props) => {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+            <figure className="px-10 pt-10">
+                <img src="/HTML.png" alt="Shoes" className="rounded-xl w-24" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{props.name} Code Snippets</h2>
+                <p>You can get Infinite {props.name}.Click on the button below</p>
+                <div className="card-actions">
+                    <button className="btn btn-warning">{props.name} Snippets</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryCard
