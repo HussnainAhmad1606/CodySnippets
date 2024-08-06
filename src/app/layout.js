f98faf2a1979c2f45111a5fca08cb3b782e3782f
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-08-06 13:40:36 +0500

    Add Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import HeroSection from "@/components/HeroSection";
 import {Toaster} from "react-hot-toast"
 import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata = {
   title: "Welcome to CodySnippets",
   description: "A collection of code snippets for web developers.",
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html data-theme="custom" lang="en">
       <body className={inter.className}>
+        <Analytics/>
         <Toaster className="z-10" />
         <Navbar />
 
